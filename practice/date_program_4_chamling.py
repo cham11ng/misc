cf9dbbc9cefbad36b3f258dfe8e9b028ae621944
Sagar Chamling <sgr.raee@gmail.com>
2023-09-18 22:04:19 -0400

    Add program 4 sample

---
+++ practice/date_program_4_chamling.py
@@ -0,0 +1,48 @@
+"""
+Module: Practice 4: Python: Python - Sagar Chamling Rai
+The average income of a Cyber security professional in
+
+USA = US$55,000
+Canada = CAD$72,000
+Europe is = 40,000 Euros and
+Lebanon = 600,000,000 LBP
+
+Assume this information is used by a professional to migrate to above four countries.
+So, allow only four countries choice of migration and consider that the user
+is in an Asian country, use conversion ratio is 1US$ = 50 Rs of that Asian country.
+
+Use this info and convert salary to print on screen that:
+You will be rich in Lebanon with salary ………………. Or
+You will be poor in Canada with salary ………………. Or
+
+Output:
+Pls enter your salary in an Asian country = 100,000 Rs
+You want to migrate to …………………. (country name – only 4 above countries)
+
+e.g. if entered choice is USA then the message would be:
+You would be poor in ……(USA)…………. With salary …………
+
+e.g. if entered choice is Lebanon then the message would be:
+You would be rich in ……. with salary …………….
+"""
+
+import os
+
+os.system("clear")
+
+
+def ask_inputs():
+    """
+    Input from user.
+    """
+
+
+def main():
+    """
+    Main function.
+    """
+    ask_inputs()
+
+
+if __name__ == "__main__":
+    main()  # Calls main function.
