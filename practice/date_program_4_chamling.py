7f7a10af4b0c379554de9b0e54cede8a11f4d436
Sagar Chamling <sgr.raee@gmail.com>
2023-09-19 08:08:31 -0400

    Add program for conversion comparison

--- practice/date_program_4_chamling.py
+++ practice/date_program_4_chamling.py
@@ -30,18 +30,71 @@ import os
 
 os.system("clear")
 
+USA = "USA"
+CANADA = "Canada"
+EUROPE = "Europe"
+LEBANON = "Lebanon"
 
-def ask_inputs():
+US_SALARY = 55_000
+CANADA_SALARY = 72_000
+EUROPE_SALARY = 40_000
+LEBANON_SALARY = 600_000_000
+
+
+def run_user_interaction():
     """
-    Input from user.
+    Run user interaction.
     """
+
+    salary = int(input("Please enter your salary in an Asian country (Rs): "))
+    convert_to = input("You want to migrate to (USA/Canada/Europe/Lebanon): ")
+
+    if convert_to in [USA, CANADA, EUROPE, LEBANON]:
+        return salary, convert_to
+    else:
+        print("Error: Country not allowed:", convert_to)
+        run_user_interaction()
+
+
+def evaluate_salary(salary, convert_to):
+    """
+    Evaluate salary
+
+    1 USD       => 50 Rs
+    $55000 USD  => 50 * 55000 Rs
+        -> $72,000 CAD  => $55,000 USD
+        -> $72,000 CAD  => Rs 50 * 55,000
+        -> $1 CAD       => Rs 50 * 55,000 / 72,000
+    """
+    us_rate = 50
+
+    if convert_to.lower() == USA.lower():
+        converted_salary = salary / us_rate
+    elif convert_to.lower() == CANADA.lower():
+        canada_rate = US_SALARY / CANADA_SALARY * us_rate
+        converted_salary = salary / canada_rate
+    elif convert_to.lower() == EUROPE.lower():
+        europe_rate = US_SALARY / EUROPE_SALARY * us_rate
+        converted_salary = salary / europe_rate
+    else:
+        lebanon_rate = US_SALARY / LEBANON_SALARY * us_rate
+        converted_salary = salary / lebanon_rate
+
+    return round(converted_salary, 2)
 
 
 def main():
     """
     Main function.
     """
-    ask_inputs()
+    salary, convert_to = run_user_interaction()
+
+    converted_salary = evaluate_salary(salary, convert_to)
+
+    if converted_salary > salary:
+        print(f"You would be rich in {convert_to} with salary {converted_salary}.")
+    else:
+        print(f"You would be poor in {convert_to} with salary {converted_salary}.")
 
 
 if __name__ == "__main__":
