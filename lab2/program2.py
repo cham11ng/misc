a5d04054391c00a0b03dae6fc12be12538a74d64
Sagar Chamling <sgr.raee@gmail.com>
2023-09-28 01:32:30 -0400

    Lab 2 initial commit

---
+++ lab2/program2.py
@@ -0,0 +1,16 @@
+"""
+Module: Lab 2 - Program 2 Classwork: Python - Sagar Chamling Rai
+
+"""
+
+import os
+
+os.system("clear")
+
+
+def _():
+    """."""
+
+
+if __name__ == "__main__":
+    _()  # Calls ... function.
---
+++ lab2/program3.py
@@ -0,0 +1,16 @@
+"""
+Module: Lab 2 - Program 3 Classwork: Python - Sagar Chamling Rai
+
+"""
+
+import os
+
+os.system("clear")
+
+
+def _():
+    """."""
+
+
+if __name__ == "__main__":
+    _()  # Calls ... function.
