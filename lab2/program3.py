08ac8e74d82259dd0c3f77a8adc7d4f8206359ca
Sagar Chamling <sgr.raee@gmail.com>
2023-09-29 11:40:30 -0400

    Start from 1 to 8

--- lab2/program3.py
+++ lab2/program3.py
@@ -10,11 +10,9 @@ os.system("clear")
 
 def run_pattern():
     """Run pattern."""
-    for i in range(0, 8):             # Range 1 to total
+    for i in range(1, 8):             # Range 1 to total
         for j in range(0, i + 1):     # Range 1 to i+1
-            if i == 0:                # Condition to continue loop if i is 0
-                continue
-            elif j == 0 or i == j:    # Condition to match for borders.
+            if j == 0 or i == j:      # Condition to match for borders.
                 print("#", end="")    # By default end is new line ('\n') so overwriting with nothing as end.
             else:
                 print(" ", end="")    # Print empty for ether case.
