29b21072c203a20a85b40f0bd53297cc4faac043
Sagar Chamling <sgr.raee@gmail.com>
2023-10-10 15:05:24 -0400

    Phonebook

--- day/day9/program.py
+++ day/day9/program.py
@@ -6,7 +6,8 @@ There will be 4 options
 1. Add Phone Number
 2. Delete Phone Number
 3. Search Phone Number
-4. Quit
+4. Update Phone Number
+5. Quit
 
 If user input more than 5 phone numbers then it will stop the program and display all the phone numbers.
 The phone number digit should be exactly 10 digit.
@@ -16,82 +17,170 @@ This has to be done by dictionary.
 
 import os
 
+MAX_PHONE_NUMBER = 5
 MENU = """
-Welcome to Phone Book Center:
+Welcome to Phone Book Portal:
 
     1. Add Phone Number
     2. Delete Phone Number
     3. Search Phone Number
-    4. Quit
+    4: Update Phone Number
+    5: Display Phone Book
+    6. Quit
 
-Total PhoneBook: {total}
+Total PhoneBook Contacts: {total}
 """
 
+i = 1  # Define global variable for indexing dictionary.
+phone_book = {}  # Empty dictionary variable for phone_book.
 
-phone_book = {}
-MAX_PHONE_NUMBER = 5
+
+msg_input_search = "Enter your name or phone number to search: "
+msg_input_option = "Enter your choice: "
+msg_input_name = "Enter name: "
+msg_input_phone = "Enter phone number: "
+msg_input_any_key = "Press any key to continue..."
+
+msg_success_add = "Phone number added successfully."
+msg_success_delete = "Phone number deleted successfully."
+msg_success_update = "Phone number updated successfully."
+msg_success_quit = "Thank you for using the phone book."
+
+msg_err_invalid_option = "Invalid option. Please try again."
+msg_err_invalid_number = "You have entered the invalid phone number."
+msg_err_not_found = "Phone number not found. Please try again."
+msg_err_maximum_limit = "You've already reached the maximum limit."
 
 
-def add_phone_number(phone_book):
-    """Add phone number."""
+def take_input():
+    """Take input to phone number."""
+    name = input(msg_input_name)
+
     while True:
-        phone_number = input("Enter your phone number: ")
-        if len(phone_number) == 10:
-            name = input("Enter your name: ")
-            phone_book[phone_number] = name
-            print("Phone number added successfully.")
-            break
+        phone_number = input(msg_input_phone)
+
+        if phone_number.isdigit() and len(phone_number) == 10 and phone_number[0] != "0":
+            return (name, phone_number)
         else:
-            print("Invalid phone number. Please try again.")
+            print(msg_err_invalid_number)
 
 
-def delete_phone_number(phone_book):
+def add_phone_number():
+    """Add phone number. Check the number (shall not start with 0 and length should be 10)."""
+    global i
+
+    if len(phone_book) == MAX_PHONE_NUMBER:
+        print(msg_err_maximum_limit)
+
+        return
+
+    (name, phone_number) = take_input()
+
+    phone_book[i] = {"name": name, "phone_number": phone_number}
+    i += 1  # Increase the global index value by 1 after adding.
+
+    print(msg_success_add)
+
+
+def delete_phone_number():
     """Add delete phone number."""
-    while True:
-        phone_number = input("Enter your phone number to delete: ")
-        if phone_number in phone_book:
-            del phone_book[phone_number]
-            print("Phone number deleted successfully.")
+    found = False
+    search_input = input(msg_input_search)
+
+    for key, value in phone_book.items():
+        if search_input.lower() == value["name"].lower() or search_input == value["phone_number"]:
+            found = True
+
+            del phone_book[key]
+            print(msg_success_delete)
+
             break
-        else:
-            print("Phone number not found. Please try again.")
+
+    if not found:
+        print(msg_err_not_found)
 
 
-def search_phone_number(phone_book):
+def search_phone_number():
     """Add search phone number."""
-    while True:
-        phone_number = input("Enter your phone number to search: ")
-        if phone_number in phone_book:
-            print(f"Phone number found: {phone_book[phone_number]}")
+    found = False
+    search_input = input(msg_input_search)
+
+    for key, value in phone_book.items():
+        if search_input.lower() == value["name"].lower() or search_input == value["phone_number"]:
+            found = True
+
+            print("Name:", value["name"])
+            print("Phone number:", value["phone_number"])
+
             break
-        else:
-            print("Phone number not found. Please try again.")
+
+    if not found:
+        print(msg_err_not_found)
+
+
+def update_phone_number():
+    """Update phone number detail, search by phone number or name."""
+    search_input = input(msg_input_search)
+    found = False
+
+    for key, value in phone_book.items():
+        if search_input.lower() == value["name"].lower() or search_input == value["phone_number"]:
+            found = True
+
+            (name, phone_number) = take_input()
+
+            phone_book[key] = {"name": name, "phone_number": phone_number}
+
+            break
+
+    if not found:
+        print(msg_err_not_found)
+
+
+def display_phone_book():
+    """Display items in a cart in table format."""
+
+    print()  # Extra line
+    print(f"{'Name':<15} {'Phone Number':<15}")  # Header of table
+    print(f"{'-'*15:<15} {'-'*15:<15}")  # Boarder of table
+
+    for value in phone_book.values():
+        print(f"{value['name']:<15} {value['phone_number']:<15}")  # Data of table
 
 
 def run_phone_book():
     """Run phone book."""
+    i = 1  # Global variable index
+
     while True:
         os.system("clear")
-        print(MENU.format(total=len(phone_book)))
+        print(MENU.format(total=len(phone_book)))  # Printing menu with total phone_book length as placeholder
 
-        user_input = input("Enter your choice: ")
+        user_input = input(msg_input_option)
 
-        print()
+        print()  # Extra line in output.
 
         if user_input == "1":
-            add_phone_number(phone_book)
+            add_phone_number()
         elif user_input == "2":
-            delete_phone_number(phone_book)
+            delete_phone_number()
         elif user_input == "3":
-            search_phone_number(phone_book)
+            search_phone_number()
         elif user_input == "4":
-            print("Thank you for using the phone book.")
+            update_phone_number()
+        elif user_input == "5":
+            display_phone_book()
+        elif user_input == "6":
+            print(msg_success_quit)
+
             break
         else:
-            print("Invalid input. Please try again.")
+            print(msg_err_invalid_option)
 
-        input("\nPress enter to continue...")
+        print()  # Extra line in output.
+
+        input(msg_input_any_key)
 
 
 if __name__ == "__main__":
-    run_phone_book()  # Calls _ function.
+    run_phone_book()  # Calls run_phone_book function.
