9b5e227af3f8f2fab3e3684a9ae57f39bba7155f
Sagar Chamling <sgr.raee@gmail.com>
2024-02-18 01:13:59 -0500

    Minor change

--- python/sample.py
+++ python/sample.py
@@ -1,4 +1,13 @@
-"""Module: Python - Sagar Chamling Rai"""
+"""
+Module: Python - Sagar Chamling Rai
+
+To create a package:
+- Create a folder with the package name
+- Create an empty __init__.py file
+- Create the required modules
+- In the __init__.py file, add code to reference the modules needed in the package
+    - from . import function_1
+"""
 
 import os
 
