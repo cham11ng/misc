66710f983dfdb9d9e4986118c4c7fb1858bb4ed7
Sagar Chamling <sgr.raee@gmail.com>
2024-03-14 22:55:36 -0400

    Update flag scripts

--- bash/ctf-2024/first-flag.sh
+++ bash/ctf-2024/first-flag.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+FLAG_PREFIX=testCTF
+FLAG_FTP=FTPNotSecure
+ROOT_PASS=CTF2024
+FLAW_USER=john
+FLAW_PASS=password12345
+LOGIN_USER=ctf-2024
+LOGIN_PASS=trios
 
 # Check if $USER is not root
 if [ "$USER" != "root" ]; then
-    echo "Please run as root."
+    echo "Please run as `root`."
     exit 1
 fi
 
 echo "First part of flag is:"
 echo
 
-# Check if ctf-2024 user exists in password file exit
-if grep -q "ctf-2024" /etc/passwd; then
+# Check if LOGIN_USER user exists in password file exit
+if grep -q "${LOGIN_USER}" /etc/passwd; then
     # Print first flag with $USER
-    echo "triOS{$USER-to-"
+    echo "${FLAG_PREFIX}{$USER-to-"
     exit 0
 fi
 
@@ -23,56 +30,58 @@ fi
 # This is to prevent other users from reading, writing, or executing
 sudo chmod go-rwx /home/* 
 
-# Change password silently to triOSCTF2024
-echo "root:triOSCTF2024" | chpasswd
-echo "kali:triOSCTF2024" | chpasswd
+# Change password silently to ${ROOT_PASS}
+echo "root:${ROOT_PASS}" | chpasswd
+echo "kali:${ROOT_PASS}" | chpasswd
 
-# ---- Setting up user "john" ----
+# ---- Setting up user "FLAW_USER" ----
 
-# Delete user john with home directory if exists
-# Check john user if exists in password file
-if grep -q "john" /etc/passwd; then
-    # Delete user john
-    userdel john
-    rm -rf /home/john
+# Delete user FLAW_USER with home directory if exists
+# Check FLAW_USER user if exists in password file
+if grep -q "${FLAW_USER}" /etc/passwd; then
+    # Delete user FLAW_USER
+    userdel ${FLAW_USER}
+    rm -rf /home/${FLAW_USER}
 fi
 
-# Add user john
-# Set default shell for john to bash
-# Set the ownership of the .ssh directory to john
-# Change the password for the user john
-useradd -m john
-usermod -s /bin/bash john
-echo "john:password12345" | chpasswd
+# Add user FLAW_USER
+# Set default shell for FLAW_USER to bash
+# Set the ownership of the .ssh directory to FLAW_USER
+# Change the password for the user FLAW_USER
+useradd -m ${FLAW_USER}
+usermod -s /bin/bash ${FLAW_USER}
+echo "${FLAW_USER}:${FLAW_PASS}" | chpasswd
+
+# Generate ssh on current directy and copy to FLAW_USER's home directory
+mkdir /home/${FLAW_USER}/.ssh
+ssh-keygen -t rsa -f "/home/${FLAW_USER}/.ssh/id_rsa" -q -N "secret"
+chown -R ${FLAW_USER}:${FLAW_USER} /home/${FLAW_USER}/.ssh
+
+# --- Installing packages ---
 
 # Installing ufw, apache2, vsftpd.
 apt-get -qqq update
 apt-get -qqq install -y ufw vsftpd apache2
 
-# --- Configuring FTP ---
+# --- Misconfiguring FTP ---
 # Create a directory for FTP user
 mkdir /var/ftp
 cp /etc/vsfptd.conf /etc/vsftpd.conf.bak
 sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd.conf
 sed -i 's/#anon_upload_enable=YES/anon_upload_enable=YES/g' /etc/vsftpd.conf
-echo "triOS{FTP-Was-Not-Secure}" > /var/ftp/flag.txt
+echo "${FLAG_PREFIX}{${FLAG_FTP}}" > /var/ftp/flag.txt
 chown nobody:nogroup /var/ftp/*
-
-# Generate ssh on current directy and copy to john's home directory
-mkdir /home/john/.ssh
-ssh-keygen -t rsa -f "/home/john/.ssh/id_rsa" -q -N "secret"
-chown -R john:john /home/john/.ssh
 
 # --- Setting up services ---
 
-# Start ssh, ftp service in Kali linux
+# Enabling services
 systemctl enable ssh
 systemctl enable vsftpd
-service ssh start
-service vsftpd start
-
-# enable service of apache2
 systemctl enable apache2
+
+# Start services
+systemctl start ssh
+systemctl start vsftpd
 systemctl start apache2
 
 # --- Setting up firewall ---
@@ -82,16 +91,15 @@ ufw allow http
 ufw allow 20/tcp
 ufw allow 21/tcp
 
-# --- Setting up CTF-2024 user ---
+# --- Setting up LOGIN_USER user ---
 
-# Add user ctf-2024
-# Set default shell for john to bash
-# Change the password for the user ctf-2024 to trios
-useradd -m ctf-2024
-usermod -s /bin/bash ctf-2024
-echo "ctf-2024:trios" | chpasswd
+# Add user LOGIN_USER
+# Set default shell for LOGIN_USER to bash
+useradd -m ${LOGIN_USER}
+usermod -s /bin/bash ${LOGIN_USER}
+echo "${LOGIN_USER}:${LOGIN_PASS}" | chpasswd
 
 # --- Print flag ---
 
 # Print first flag with $USER
-echo "triOS{$USER-to-"
+echo "${FLAG_PREFIX}{$USER-to-"
--- bash/ctf-2024/second-flag.sh
+++ bash/ctf-2024/second-flag.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+LOGIN_USER=ctf-2024
 
-# if $USER is not ctf-2024, exit
-if [ "$USER" != "ctf-2024" ]; then
-    echo "Please run as ctf-2024."
+# if $USER is not LOGIN_USER, exit
+if [ "$USER" != "$LOGIN_USER" ]; then
+    echo "Please run as $LOGIN_USER user."
     exit 0
 fi
 
